166b4a80865cfa74a4e3b192ea63a353bce763a6
Swarup <suryawanshiswarup@gamil.com>
2023-04-25 09:16:01 +0530

    float point precision

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let is_true = true;
-    let my_grade = 'A';
-
-    println!("{} {}", is_true, my_grade)
+    let num_1: f32 = 1.111111111111111;
+    println!("f32: {}", num_1 + 0.111111111111111); //  1.2222223
+    let num_2: f64 = 1.111111111111111;
+    println!("f64: {}", num_2 + 0.111111111111111); // 1.2222222222222219
 }
