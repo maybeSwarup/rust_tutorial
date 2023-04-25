ba328c299f8d351ee6f8205afdc85339cd8c2358
Swarup <suryawanshiswarup@gamil.com>
2023-04-25 09:22:15 +0530

    random number

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,6 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let mut num_3: u32 = 5;
-    let num_4: u32 = 4;
-    println!("5 + 4 = {}", num_3 + num_4);
-    println!("5 - 4 = {}", num_3 - num_4);
-    println!("5 * 4 = {}", num_3 * num_4);
-    println!("5 / 4 = {}", num_3 / num_4);
-    println!("5 % 4 = {}", num_3 % num_4);
-    num_3 += 1;
-    println!("\nnum_3 = {}", num_3);
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random: {}", random_num)
 }
