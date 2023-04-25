ba2706fb11dda62e78ea48858697741c7def0cd3
Swarup <suryawanshiswarup@gamil.com>
2023-04-25 09:20:55 +0530

    basic math

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let num_1: f32 = 1.111111111111111;
-    println!("f32: {}", num_1 + 0.111111111111111); //  1.2222223
-    let num_2: f64 = 1.111111111111111;
-    println!("f64: {}", num_2 + 0.111111111111111); // 1.2222222222222219
+    let mut num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+    num_3 += 1;
+    println!("\nnum_3 = {}", num_3);
 }
