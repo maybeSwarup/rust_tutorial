c3441cf7682c667826167f5f59383a3d86bb6361
Swarup <suryawanshiswarup@gamil.com>
2023-04-25 09:12:43 +0530

    boolean and char

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    println!("Max u32: {}", u32::MAX);
-    println!("Max u64: {}", u64::MAX);
-    println!("Max usize: {}", usize::MAX);
-    println!("Max u128: {}", u128::MAX);
-    println!("Max f32: {}", f32::MAX);
-    println!("Max f64: {}", f64::MAX);
+    let is_true = true;
+    let my_grade = 'A';
+
+    println!("{} {}", is_true, my_grade)
 }
