427cd10975acebbaa0c6d1c22512993d52dbf74f
Swarup <suryawanshiswarup@gamil.com>
2023-05-08 22:19:35 +0530

    else if

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let random_num = rand::thread_rng().gen_range(1..101);
-    println!("Random: {}", random_num)
+    let age = 19;
+
+    if (age >= 1) && (age <= 18) {
+        println!("Important Birthday")
+    } else if (age == 21) || (age == 50) {
+        println!("Not Much Important Birthday")
+    } else if age > 65 {
+        println!("You are definitely not a child anymore!")
+    } else {
+        println!("You are possibly/presumed dead person.")
+    }
 }
