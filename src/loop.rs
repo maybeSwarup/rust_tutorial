47b120c06a44e4a5a9e9a37e2e5d5656af3ffaa5
Swarup <suryawanshiswarup@gamil.com>
2023-05-09 10:37:47 +0530

    while loop

--- src/loop.rs
+++ src/loop.rs
@@ -9,9 +9,16 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 fn main() {
     let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut loop_idx = 0;
+    loop {
+        if arr_2[loop_idx] % 2 == 0 {
+            loop_idx += 1;
+            continue;
+        }
+        if arr_2[loop_idx] == 9 {
+            break;
+        }
 
-    while loop_idx < arr_2.len() {
-        println!("Arr: {}", arr_2[loop_idx]);
+        println!("val: {}", arr_2[loop_idx]);
         loop_idx += 1;
     }
 }
