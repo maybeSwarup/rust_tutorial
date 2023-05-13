a76b633599105f13f86dfb495bfedf97f39a5da1
Swarup <suryawanshiswarup@gamil.com>
2023-05-14 01:10:26 +0530

    function

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,10 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
+fn get_sum(x: i32, y: i32) {
+    println!("{} + {} = {}", x, y, x + y)
+}
+
 fn main() {
-    let vec1: Vec<i32> = Vec::new();
-    let mut vec2 = vec![1, 2, 3, 4];
-    vec2.push(5);
-    println!("1st: {}", vec2[0]);
-    let second: &i32 = &vec2[1];
-
-    match vec2.get(1) {
-        Some(second) => println!("2nd: {}", second),
-        None => println!("No 2nd value"),
-    }
-
-    for i in &mut vec2 {
-        *i *= 2;
-    }
-
-    for i in &vec2 {
-        println!("{}", i);
-    }
-
-    println!("Vec Length {}", vec2.len());
-    println!("Pop: {:?}", vec2.pop());
+    get_sum(5, 4);
 }
