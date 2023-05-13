da069dfd2ec07f2d3e84522907cf4f50e1080d43
Swarup <suryawanshiswarup@gamil.com>
2023-05-14 02:22:07 +0530

    struct

---
+++ src/examples/HashMap.rs
@@ -0,0 +1,28 @@
+#![allow(unused)] // ignore unused variable warnings
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("Flash", "Barry Allen");
+
+    for (k, v) in heroes.iter() {
+        println!("{} {}", k, v);
+    }
+
+    if heroes.contains_key(&"Batman") {
+        let the_batman = heroes.get(&"Batman");
+
+        match the_batman {
+            Some(x) => println!("Batman is a hero"),
+            None => println!("Batman is not a hero"),
+        }
+    }
+}
