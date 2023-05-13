5f25800e8fe22a6fea4a204f60555fd323f9b264
Swarup <suryawanshiswarup@gamil.com>
2023-05-13 14:14:47 +0530

    type casting with as

---
+++ src/examples/string_operations.rs
@@ -0,0 +1,35 @@
+#![allow(unused)] // ignore unused variable warnings
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    let st3 = String::from("x r t b h k k a n c");
+    let mut v1: Vec<char> = st3.chars().collect();
+
+    v1.sort();
+    v1.dedup(); // remove duplicates
+
+    for char in v1 {
+        println!("{}", char);
+    }
+
+    let st4: &str = "Random string";
+    let mut st5: String = st4.to_string();
+    println!("{}", st5);
+
+    let byte_arr1 = st5.as_bytes();
+    let st6 = &st5[0..6];
+    println!("String {} length {}", st6, st6.len());
+    st5.clear();
+
+    let st6 = String::from("Just some");
+    let st7 = String::from(" words");
+    let st8 = st6 + &st7;
+    for char in st8.bytes() {
+        println!("{}", char);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,9 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let st3 = String::from("x r t b h k k a n c");
-    let mut v1: Vec<char> = st3.chars().collect();
+    let int_u8: u8 = 5;
+    let int2_u8: u8 = 4;
+    let int3_u32: u32 = (int_u8 as u32) + (int_u8 as u32);
 
-    v1.sort();
-    v1.dedup(); // remove duplicates
-
-    for char in v1 {
-        println!("{}", char);
-    }
-
-    let st4: &str = "Random string";
-    let mut st5: String = st4.to_string();
-    println!("{}", st5);
-
-    let byte_arr1 = st5.as_bytes();
-    let st6 = &st5[0..6];
-    println!("String {} length {}", st6, st6.len());
-    st5.clear();
-
-    let st6 = String::from("Just some");
-    let st7 = String::from(" words");
-    let st8 = st6 + &st7;
-    for char in st8.bytes() {
-        println!("{}", char);
-    }
+    println!("{} + {} = {}", int_u8, int2_u8, int3_u32);
 }
