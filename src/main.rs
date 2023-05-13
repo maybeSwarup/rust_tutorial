0b87aa5e9d26a811a728bd78ec4870f62c0c4a3f
Swarup <suryawanshiswarup@gamil.com>
2023-05-14 01:35:29 +0530

    clone

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,8 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
-use std::ops::Add;
-
-fn get_sum_gen<T: Add<Output = T>>(x: T, y: T) -> T {
-    return x + y;
-}
-
 fn main() {
-    println!("5 + 4 = {}", get_sum_gen(5, 4));
-    println!("5.2 + 4.6 = {}", get_sum_gen(5.2, 4.6));
+    let str1 = String::from("world");
+    let str2 = str1.clone();
+    println!("Hello {}", str1);
 }
