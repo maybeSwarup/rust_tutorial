919d99f905c7505820b1c4b9b4de3704875f44da
Swarup <suryawanshiswarup@gamil.com>
2023-05-14 01:30:56 +0530

    add trait T

---
+++ src/examples/sum_of_list.rs
@@ -0,0 +1,20 @@
+#![allow(unused)] // ignore unused variable warnings
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn sum_list(list: &[i32]) -> i32 {
+    let mut sum = 0;
+    for &val in list.iter() {
+        sum += &val;
+    }
+    sum
+}
+
+fn main() {
+    let num_list = vec![1, 2, 3, 4, 5];
+    println!("Sum of list = {}", sum_list(&num_list))
+}
