7c8110d977835335beb64adc70a599b779692b27
Swarup <suryawanshiswarup@gamil.com>
2023-05-27 10:47:09 +0530

    panic

---
+++ src/examples/restaurant_module.rs
@@ -0,0 +1,15 @@
+#![allow(unused)] // ignore unused variable warnings
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+mod restaurant;
+use crate::restaurant::order_food;
+
+fn main() {
+    order_food();
+}
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,37 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
-mod restaurant;
-use crate::restaurant::order_food;
-
 fn main() {
-    order_food();
+    let path = "files/lines.txt";
+
+    let output = File::create(path);
+
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file: {:?}", error);
+        }
+    };
+
+    write!(output, "Just some\nRandom words").expect("Failed to write to file");
+
+    let input = File::open(path).unwrap();
+
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
+
+    let output2 = File::create("files/random text.txt");
+    let output2 = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("files/rand.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Can't create file: {:?}", error),
+            },
+            _other_error => panic!("Problem opening file: {:?}", error),
+        },
+    };
 }
